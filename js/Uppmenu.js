444fdae5cf2f27d56f24784185844361ae65406e
bluerat1976 <osoblivaia76@mail.ru>
2020-04-08 23:04:44 +0300

    in process

--- js/Uppmenu.js
+++ js/Uppmenu.js
@@ -1,13 +1,16 @@
 class UpperMenuItem {
-    constructor(name, id, href,) {
-        this.name = name
+    constructor(id, name, href,) {
         this.id = id
+        this.name = name
         this.href = href
 
     }
 
 
     render() {
+        let linkUpNav = document.createElement('a')
+        linkUpNav.addClassList('appnav' + this.id)
+        
 
     }
 }
@@ -15,5 +18,9 @@ class UpperMenuItem {
 //-------------------------
 
 let uppermenu = [
-    new UpperMenuItem('Home', )
+    new UpperMenuItem(10, 'Home', 'index.html'),
+    new UpperMenuItem(20, 'About me', 'aboutme.html'),
+    new UpperMenuItem(30, 'Portfolio', 'portfolio.html'),
+    new UpperMenuItem(40, 'Blog', 'blog.html'),
+    new UpperMenuItem(50, 'CV', 'cv.html'),
 ]
