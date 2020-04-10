fc2d9897b7bda0cd58feb95bf81ec9788b8fd2ce
bluerat1976 <osoblivaia76@mail.ru>
2020-04-11 01:54:32 +0300

    in process

---
+++ js/Header.js
@@ -0,0 +1,56 @@
+let header = document.getElementById('hd')
+
+let boxlogo = document.getElementById('box-logo')
+
+let logo = document.createElement('h3')
+logo.innerHTML = `Blin\nFignia`
+boxlogo.appendChild(logo)
+
+let logopict = document.createElement('img')
+logopict.setAttribute('src', 'picts/upmenu/upmenu-9.jpg')
+logopict.setAttribute('id', 'pict-logo')
+boxlogo.appendChild(logopict)
+
+header.appendChild(boxlogo)
+
+
+
+//--------------------------------------------------
+class UpperMenuItem {
+    constructor(id, name, href) {
+        this.id = id
+        this.name = name
+        this.href = href
+    }
+
+    render() {
+        let linkUpNav = document.createElement('a')
+        linkUpNav.setAttribute ('id', this.id)
+        linkUpNav.setAttribute('href', this.href)
+        linkUpNav.innerText = this.name
+
+        return linkUpNav
+    }
+}
+
+//-------------------------
+
+let uppermenu = [
+    new UpperMenuItem('uppnav1', 'Home', 'index.html'),
+    new UpperMenuItem('uppnav2', 'About me', 'aboutme.html'),
+    new UpperMenuItem('uppnav3', 'Portfolio', 'portfolio.html'),
+    new UpperMenuItem('uppnav4', 'Blog', 'blog.html'),
+    new UpperMenuItem('uppnav5', 'CV', 'cv.html'),
+]
+
+function renderUpNav() {
+    let wrapper = document.getElementById('box-nav')
+
+    uppermenu.forEach( 
+        menulist => wrapper.appendChild(menulist.render())
+    ) 
+    header.appendChild(wrapper)
+}
+
+renderUpNav('#box-nav')
+
